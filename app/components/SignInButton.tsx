d4740235c1999b1d6be1c545596519900c2aae5c
JesseSinivuori <sinvuorii@gmail.com>
2023-09-16 00:47:39 +0300

    - toast from signin/out

--- app/components/SignInButton.tsx
+++ app/components/SignInButton.tsx
@@ -1,23 +1,12 @@
 "use client";
 import { signIn } from "next-auth/react";
-import toast from "react-hot-toast";
 
 export default function SignInButton() {
-  const handleSignIn = async () => {
-    const signInPromise = signIn("github", { redirect: false });
-
-    toast.promise(signInPromise, {
-      loading: "Signing in...",
-      success: "Signed in!",
-      error: "Sign in failed. Please try again.",
-    });
-  };
-
   return (
     <button
       type="button"
       className="flex whitespace-nowrap py-2 px-4 dark:bg-green-700 bg-green-600 text-white rounded-md border dark:border-green-700 border-green-600"
-      onClick={handleSignIn}
+      onClick={() => signIn("github", { redirect: false })}
     >
       Sign in with GitHub
     </button>
--- app/components/SignOutButton.tsx
+++ app/components/SignOutButton.tsx
@@ -1,23 +1,12 @@
 "use client";
 import { signOut } from "next-auth/react";
-import toast from "react-hot-toast";
 
 export default function SignOutButton() {
-  const handleSignOut = async () => {
-    const signOutPromise = signOut();
-
-    toast.promise(signOutPromise, {
-      loading: "Signing out...",
-      success: "Signed out!",
-      error: "Sign out failed. Please try again.",
-    });
-  };
-
   return (
     <button
       type="button"
       className="flex whitespace-nowrap py-2 px-4 rounded-md border dark:border-white/10 border-black/10"
-      onClick={handleSignOut}
+      onClick={() => signOut()}
     >
       Sign out
     </button>
