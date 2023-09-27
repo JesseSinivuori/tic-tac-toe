b756933f9b577fba4edd3b9ad54f979cdb347ae5
JesseSinivuori <sinvuorii@gmail.com>
2023-09-27 16:33:20 +0300

    refactor, username style

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,23 +2,23 @@ import NextAuth, { type NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+const clientId =
+  process.env.NODE_ENV === "production"
+    ? process.env.GITHUB_ID!
+    : process.env.GITHUB_ID_DEV!;
+const clientSecret =
+  process.env.NODE_ENV === "production"
+    ? process.env.GITHUB_SECRET!
+    : process.env.GITHUB_SECRET_DEV!;
+
 export const authOptions: NextAuthOptions = {
   // Secret for Next-auth, without this JWT encryption/decryption won't work
   secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: "/sign-in",
-  },
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId:
-        process.env.NODE_ENV === "production"
-          ? process.env.GITHUB_ID!
-          : process.env.GITHUB_ID_DEV!,
-      clientSecret:
-        process.env.NODE_ENV === "production"
-          ? process.env.GITHUB_SECRET!
-          : process.env.GITHUB_SECRET_DEV!,
+      clientId: clientId,
+      clientSecret: clientSecret,
     }),
   ],
   callbacks: {
--- app/components/navbar/UserName.tsx
+++ app/components/navbar/UserName.tsx
@@ -4,7 +4,7 @@ import { useUser } from "@/app/providers/UserProvider";
 export function UserName({ ...props }: React.ComponentProps<"div">) {
   const { user } = useUser();
   return (
-    <div className="flex" {...props}>
+    <div className="flex text-zinc-950/90 dark:text-zinc-50/90" {...props}>
       {user?.username}
     </div>
   );
