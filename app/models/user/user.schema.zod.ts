cd7d3a219ac80d226a7d9f6ef9999bf9cac43ed0
JesseSinivuori <sinvuorii@gmail.com>
2023-10-04 16:42:27 +0300

    fixed type error

--- app/models/user/user.schema.zod.ts
+++ app/models/user/user.schema.zod.ts
@@ -40,7 +40,7 @@ export const gameHistorySchema = z
     wins: z.number(),
     losses: z.number(),
     draws: z.number(),
-    games: z.array(gameSchema).or(z.array(idSchema)),
+    games: z.array(gameSchema),
   })
   .optional();
 
